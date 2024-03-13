1c82a552cffa64623f671fb58ab94947f30ed207
mohamed khalid <mohamed.khalid.gamal@gmail.com>
2024-03-13 05:01:23 +0200

    shop page

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,49 @@
 let navs = document.querySelectorAll("#header .navbar li");
-let shop = document.querySelector("#hero a");
-let contact = document.getElementById("contactButton");
-let about = document.getElementById("aboutButton");
-let cart = document.getElementById("cartButton");
 let bars = document.querySelector("#header .bar");
 let close = document.querySelector("#header .navbar .close");
 let nav = document.querySelector("#header .navbar");
-navs.forEach((e) => {
-  e.addEventListener("click", function () {
-    navs.forEach((l) => l.classList.remove("active"));
-    e.classList.add("active");
+let products = document.querySelectorAll(".products .pro");
+let proimg = document.querySelector("#pro-details .images .basic");
+let imgs = document.querySelectorAll("#pro-details .images .alters img");
+if (bars || close) {
+  bars.addEventListener("click", function () {
+    nav.style.width = "40%";
   });
-});
-navActive(shop, 1);
-navActive(contact, 4);
-navActive(about, 3);
-navActive(cart, 5);
-function navActive(x, i) {
-  x.addEventListener("click", function () {
-    navs.forEach((x) => x.classList.remove("active"));
-    navs[i].classList.add("active");
+  close.addEventListener("click", function () {
+    nav.style.width = "0%";
   });
 }
-bars.addEventListener("click", function () {
-  nav.style.width = "40%";
+
+for (i = 0; i < 6; i++) {
+  if (location.pathname.includes(navs[i].textContent.toLocaleLowerCase())) {
+    navs.forEach((x) => x.classList.remove("active"));
+    navs[i].classList.add("active");
+    break;
+  } else if (location.pathname.includes("index") || location.pathname == "/") {
+    navs.forEach((x) => x.classList.remove("active"));
+    navs[0].classList.add("active");
+    break;
+  } else {
+    navs.forEach((x) => x.classList.remove("active"));
+    navs[1].classList.add("active");
+    break;
+  }
+}
+products.forEach((e) => {
+  e.addEventListener("click", function () {
+    x = e.childNodes[1].childNodes[1].src;
+    location.href = "sproduct.html?imgsrc=" + x;
+  });
 });
-close.addEventListener("click", function () {
-  nav.style.width = "0%";
-});
+if (proimg) {
+  var pr = new URLSearchParams(location.search);
+  var sorc = pr.get("imgsrc");
+  proimg.src = sorc;
+}
+if (imgs) {
+  imgs.forEach((e) => {
+    e.addEventListener("click", function () {
+      proimg.src = e.src;
+    });
+  });
+}
